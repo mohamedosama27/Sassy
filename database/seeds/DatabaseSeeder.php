9ce7e5c7cedac33d1c7f1113055ce67223f30a1d
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-23 01:43:10 +0200

    changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        //factory(item::class,9)->create();
-        factory(User::class,1)->create();
+        factory(item::class,20)->create();
+        // factory(User::class,1)->create();
     }
 }
